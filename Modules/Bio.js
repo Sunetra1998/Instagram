9fe60a492e63f8fa03f923024d8fe957193de081
Sunetra1998 <73545503+Sunetra1998@users.noreply.github.com>
2021-03-04 15:51:47 +0530

    Add files via upload

---
+++ Modules/Bio.js
@@ -0,0 +1,13 @@
+const Bio = () => `
+        <section class="bio">
+            <div class="profile-photo">
+                <img src="https://i.pinimg.com/originals/3f/dd/7e/3fdd7e16f26594a36e514f4b1cd4140e.jpg" alt="">
+            </div>
+            <div class="profile-info">
+                <p class="name">Sunetra.Das</p>
+                <button>follow</button>
+            </div>
+        </section>
+        `;
+
+export default Bio;
---
+++ Modules/Gallery.js
@@ -0,0 +1,28 @@
+const Gallery = () => `
+        <input type="file" name="photo" id="addPhotoInput">
+        <label for="addPhotoInput">
+            <i class="add-photo fas fa-plus-square"></i>
+        </label>
+        <div class="gallery-nav"></div>
+        <section class="gallery">
+            <a href="#" class="item">
+                <img src="https://i.pinimg.com/originals/03/e2/0c/03e20ca2cab9b1c834657215a21438db.jpg" alt="">
+            </a>
+            <a href="#" class="item">
+                <img src="https://i1.wp.com/www.kahanihindi.com/wp-content/uploads/2020/02/Whatsapp-DP-images-9.jpg?resize=450%2C400&ssl=1" alt="">
+            </a>
+            <a href="#" class="item">
+                <img src="https://i.pinimg.com/originals/99/66/6b/99666b2c752d042116ee036467aabe64.jpg" alt="">
+            </a>
+            <a href="#" class="item">
+                <img src="https://i.pinimg.com/originals/b2/63/61/b26361a9522fb837ced186fbd4187eea.jpg" alt="">
+            </a>
+            <a href="#" class="item">
+                <img src="https://image.freepik.com/free-vector/i-love-u-background-with-love-illustration-text-pink-background_112769-366.jpg" alt="">
+            </a>
+            <a href="#" class="item">
+                <img src="https://i.pinimg.com/originals/59/1e/fb/591efba3276e85c5d543ed07f3283f93.png" alt="">    
+            </a>
+        </section>
+`;
+export default Gallery;
